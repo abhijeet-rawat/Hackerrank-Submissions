c30c7cbddcc59d380a9d6b266c701f6ad5ab9d82
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:22:35 +0530

    added Simple Array Sum

---
+++ submissions/Simple Array Sum.cpp
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Simple Array Sum
+Problem Link: https://www.hackerrank.com/challenges/simple-array-sum
+Author: abhijeet_rawat
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, sum;
+    cin >> n;
+    vector<int> arr(n);
+    sum = 0;
+    for (int arr_i = 0; arr_i < n; arr_i++) {
+        cin >> arr[arr_i];
+        sum += arr[arr_i];
+    }
+    cout << sum;
+    return 0;
+}
