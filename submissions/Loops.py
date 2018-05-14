49e3026ebd12effdac5907b8cea6a3f7bc6e0fac
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:18:18 +0530

    added Loops

---
+++ submissions/Loops.py
@@ -0,0 +1,13 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Loops
+Problem Link: https://www.hackerrank.com/challenges/python-loops
+Author: abhijeet_rawat
+Language : Python 3
+
+-----------------------------------------------------------------------'''
+
+
+n = int(input())
+for i in range(0,n,1):
+    print(i**2)
