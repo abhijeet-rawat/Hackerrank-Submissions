37c5cf9230263a5d6ac57ca379b07bed70a170a5
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:18:26 +0530

    added Arithmetic Operators

---
+++ submissions/Arithmetic Operators.py
@@ -0,0 +1,15 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Arithmetic Operators
+Problem Link: https://www.hackerrank.com/challenges/python-arithmetic-operators
+Author: abhijeet_rawat
+Language : Python 3
+
+-----------------------------------------------------------------------'''
+
+
+a=int(input())
+b=int(input())
+print(a+b)
+print(a-b)
+print(a*b)
