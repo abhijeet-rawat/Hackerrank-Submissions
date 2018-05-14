a823e88c70cd2ff6c262a9dd4d09655e8337a24e
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:23:39 +0530

    updated Compare two linked lists

--- submissions/Compare two linked lists.cpp
+++ submissions/Compare two linked lists.cpp
@@ -20,18 +20,23 @@ Language : C++
 */
 int CompareLists(Node* headA, Node* headB)
 {
-    struct Node *ptr1 = headA, *ptr2 = headB;
-    int flag = 1;
+    int flag;
+    struct Node *ptr1, *ptr2;
+    ptr1 = headA;
+    ptr2 = headB;
+    flag = 0;
     while (ptr1 != NULL && ptr2 != NULL) {
-        if (ptr1->data == ptr2->data) {
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
+        if ((ptr1->data) != (ptr2->data)) {
+            flag = 1;
+            break;
         }
-        else {
-            return 0;
-        }
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
     }
-    if ((ptr1 == NULL && ptr2 != NULL) || (ptr1 != NULL && ptr2 == NULL)) {
+    if (flag == 1) {
+        return 0;
+    }
+    else if (ptr1 != NULL || ptr2 != NULL) {
         return 0;
     }
     else {
