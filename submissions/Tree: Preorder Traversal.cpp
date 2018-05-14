6442b5acd18f5fd4a81ce86d7ebf0cfe7fed083e
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:23:35 +0530

    added Tree: Preorder Traversal

---
+++ submissions/Tree: Preorder Traversal.cpp
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Tree: Preorder Traversal
+Problem Link: https://www.hackerrank.com/challenges/tree-preorder-traversal
+Author: abhijeet_rawat
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+
+void preOrder(node* root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
