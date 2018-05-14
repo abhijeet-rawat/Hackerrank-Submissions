9e30c8770e585417c54d505e5b4ffed14afaace6
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:20:43 +0530

    added Print in Reverse

---
+++ submissions/Print in Reverse.cpp
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Print in Reverse
+Problem Link: https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse
+Author: abhijeet_rawat
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
+/*
+  Print elements of a linked list in reverse order as standard output
+  head pointer could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+void ReversePrint(Node* head)
+{
+    if (head == NULL) {
+        return;
+    }
+    else {
+        ReversePrint(head->next);
+        cout << head->data << endl;
+    }
+}
