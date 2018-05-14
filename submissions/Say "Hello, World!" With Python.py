a5d3ba58899f8aed2e2325058cb2084797cce767
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:18:33 +0530

    added Say "Hello, World!" With Python

---
+++ submissions/Say "Hello, World!" With Python.py
@@ -0,0 +1,12 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Say "Hello, World!" With Python
+Problem Link: https://www.hackerrank.com/challenges/py-hello-world
+Author: abhijeet_rawat
+Language : Python 3
+
+-----------------------------------------------------------------------'''
+
+
+print("Hello, World!")
+
