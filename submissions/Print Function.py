516730786c48072dc89cb800ae5709b5829f0b39
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:18:06 +0530

    added Print Function

---
+++ submissions/Print Function.py
@@ -0,0 +1,15 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Print Function
+Problem Link: https://www.hackerrank.com/challenges/python-print
+Author: abhijeet_rawat
+Language : Python 3
+
+-----------------------------------------------------------------------'''
+
+
+n = int(input())
+for i in range(1,n+1,1):
+    print(i,end="")
+       
+            
