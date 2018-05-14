f74fbedcecaec80a6764d5e9e6b55bf02f4ab8fc
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:23:02 +0530

    added Maximum Draws

---
+++ submissions/Maximum Draws.cpp
@@ -0,0 +1,28 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Maximum Draws
+Problem Link: https://www.hackerrank.com/challenges/maximum-draws
+Author: abhijeet_rawat
+Language : C++
+
+-----------------------------------------------------------------------*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    long long int n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cout << n + 1 << endl;
+    }
+    return 0;
+}
