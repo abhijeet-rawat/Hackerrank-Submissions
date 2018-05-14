7d63c08092237f857fdc80c95d54bbecba6a8f19
Abhijeet Rawat <abhijeet.rawat5@gmail.com>
2018-05-14 11:18:22 +0530

    added Python: Division

---
+++ submissions/Python: Division.py
@@ -0,0 +1,14 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Python: Division
+Problem Link: https://www.hackerrank.com/challenges/python-division
+Author: abhijeet_rawat
+Language : Python 3
+
+-----------------------------------------------------------------------'''
+
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
